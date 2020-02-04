f89c547bc8e4a800c310d0e6db40d13611197d2a
agpt8 <ayush.gpt8@gmail.com>
2020-02-04 13:14:15 +0530

    simplify the functions

--- config.py
+++ config.py
@@ -18,37 +18,29 @@ import tweepy
 
 
 def create_api():
-    """This function collects the consumer and access keys and returns authenticated api object.
+    """This function collects the consumer and access keys, creates an api object and returns the authenticated api
+    object.
 
     :return: authenticated api object
-    :rtype: object
     """
     consumer_key = getenv('CONSUMER_KEY')
     consumer_secret = getenv('CONSUMER_SECRET')
     access_token = getenv('ACCESS_TOKEN')
     access_token_secret = getenv('ACCESS_TOKEN_SECRET')
 
-    return authenticate_api(consumer_key, consumer_secret, access_token, access_token_secret)
-
-
-def authenticate_api(consumer_key: str, consumer_secret: str, access_token: str, access_token_secret: str) -> object:
-    """This function authenticates the consumer and access keys and returns an api object
-
-    :param consumer_key:
-    :type consumer_key: str
-    :param consumer_secret:
-    :type consumer_secret: str
-    :param access_token:
-    :type access_token: str
-    :param access_token_secret:
-    :type access_token_secret: str
-    :return: tweepy api object
-    :rtype: object
-    """
     auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
     auth.set_access_token(access_token, access_token_secret)
     api = tweepy.API(auth)
 
+    return authenticate_api(api)
+
+
+def authenticate_api(api):
+    """This function verifies the credentials and returns the authenticated api object
+
+    :param api: api object
+    :return: authenticated api object
+    """
     try:
         api.verify_credentials()
         print("Authentication OK")
