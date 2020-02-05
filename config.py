cfee6c96f81f11060e198cd3e3b351f3cefceb07
agpt8 <ayush.gpt8@gmail.com>
2020-02-05 14:54:37 +0530

    add ability to run the script independently

--- config.py
+++ config.py
@@ -1,15 +1,3 @@
-# imports: tweepy and os
-
-
-# Define a function create_api to use os.getenv to access our tokens
-
-# Keys are called:
-# CONSUMER_KEY
-# CONSUMER_SECRET
-# ACCESS_TOKEN
-# ACCESS_TOKEN_SECRET
-
-# Use tweep OAuth to connect to the tweepy API and include a try except block to verify credentials and catch errors, return api
 from os import getenv
 
 import tweepy
@@ -45,3 +33,7 @@ def authenticate_api(api):
         return api
     except tweepy.TweepError as error:
         return error
+
+
+if __name__ == "__main__":
+    create_api()
