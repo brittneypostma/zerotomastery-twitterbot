8f03bdb4db511350e5bfc1d107dfc471c6e36cce
agpt8 <ayush.gpt8@gmail.com>
2020-02-05 19:33:18 +0530

    api will wait on wait limit of Twitter and notify

--- config.py
+++ config.py
@@ -17,7 +17,7 @@ def create_api():
 
     auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
     auth.set_access_token(access_token, access_token_secret)
-    api = tweepy.API(auth)
+    api = tweepy.API(auth, wait_on_rate_limit=True, wait_on_rate_limit_notify=True)
 
     return authenticate_api(api)
 
