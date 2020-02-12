b112c1f251763fc5324f610bc99609248f11f452
agpt8 <ayush.gpt8@gmail.com>
2020-02-12 19:38:52 +0530

    minor formatting changes

--- ztm-bot.py
+++ ztm-bot.py
@@ -1,20 +1,3 @@
-# This file is for the Streaming tweepy API
-# import time, tweepy, and the create_api function from config.py
-
-# Create a class that accepts the tweepy.StreamListener
-
-# Define an __init__ function inside the class that accepts self and api parameters
-
-# Define an on_status function inside the class that accepts self and tweet parameters, retweet and favorite if the tweet has not been already
-
-# Define an on_error function inside the class to catch errors
-
-################### END OF CLASS ########################
-
-# Define a main function that takes keywords and ids and connects to the tweepy stream api using those keywords and ids to track and follow
-
-# if __name__ main define keywords to search for and ids to follow and run the main function with those
-
 import tweepy
 
 from config import create_api
