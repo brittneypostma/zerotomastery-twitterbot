95fe553d2cfe48463ba771f2c259a0b193e4ee18
Brittney <brittney.postma@hotmail.com>
2020-02-15 15:00:52 -0500

    took out lower()

--- search.py
+++ search.py
@@ -43,7 +43,7 @@ def check_mentions(api, keywords, since_id):
     for tweet in tweepy.Cursor(api.mentions_timeline,
                                since_id=since_id).items():
         new_since_id = max(tweet.id, new_since_id)
-        if any(keyword in tweet.text.lower() for keyword in keywords):
+        if any(keyword in tweet.text for keyword in keywords):
             status = '@' + tweet.user.screen_name + \
                 ' Zero To Mastery, ZTMBot to the rescue! zerotomastery.io/'
             api.update_status(
