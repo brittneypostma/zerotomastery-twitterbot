4f05bdf516556524bf77aa4d9bcd64773a79a5d8
sballgirl11 <brittney.postma@hotmail.com>
2020-02-18 10:43:48 -0500

    refactored reply to mentions and removed the fav/retweet function

--- search.py
+++ search.py
@@ -15,10 +15,16 @@ followed_users_ids: List[str] = []
 def follow_followers(api):
     """Follow all followers."""
     for follower in tweepy.Cursor(api.followers).items():
-        if not follower.following:
-            follower.follow()
-            followed_users_ids.append(follower.id)
-            print('Followed ', follower.name)
+        try:
+            print(follower.name, 'followed the bot.')
+            if not follower.following:
+                follower.follow()
+                followed_users_ids.append(follower.id)
+                print('Followed ', follower.name)
+                time.sleep(10)
+        except tweepy.RateLimitError:
+            print("Rate Limit Exceeded")
+            time.sleep(900)
 
 
 # Todo: Rename to unfollow_non_followers?
@@ -36,63 +42,67 @@ def unfollow(api):
             print("Rate Limit Exceeded")
             time.sleep(900)
 
+def reply_to_mention(api, tweet, keywords):
+    """Reply to a non-reply mention containing any of the keywords."""
+    try:
+        if tweet.user.id == api.me().id or tweet.in_reply_to_status_id:
+            return None
+        text = tweet.text.lower()
+        print(f'Checking tweet {tweet.id} by @{tweet.user.screen_name}...')
+        if not keywords or any(keyword.lower() in text for keyword in keywords):
+            print(f'\n\nReplying to tweet {tweet.id} by @'
+                  f'{tweet.user.screen_name}...')
+            status = (f'@{tweet.user.screen_name} Zero To Mastery, ZTMBot to'
+                      ' the rescue!\nzerotomastery.io/')
+            api.update_status(status=status,
+                              in_reply_to_status_id=tweet.id_str,
+                              auto_populate_reply_metadata=True)
+            print('Replied to', tweet.user.screen_name)
+            time.sleep(900)
+    except tweepy.TweepError as e:
+        print("Error replying", e)
 
-# Define a check_mentions function that accepts api, keywords, and since_id, follow and reply to the user if user has mentioned us
-def check_mentions(api, keywords, since_id):
-    new_since_id = since_id
+def reply_to_mentions_since_id(api, keywords, since_id) -> int:
+    """Get new since_id after replying to mentions."""
     for tweet in tweepy.Cursor(api.mentions_timeline,
                                since_id=since_id).items():
-        new_since_id = max(tweet.id, new_since_id)
-        try:
-            if tweet.in_reply_to_status_id is not None:
-                # Tweet is a reply
-                break
-            elif any(keyword in tweet.text for keyword in keywords):
-                status = '@' + tweet.user.screen_name + \
-                ' Zero To Mastery, ZTMBot to the rescue! zerotomastery.io/'
-                api.update_status(
-                status=status, in_reply_to_status_id=tweet.id_str)
-                print('replied to', tweet.user.screen_name)
-                time.sleep(900)
-            else:
-                pass
-        except tweepy.TweepError as e:
-            print("Error replying", e)
-    return new_since_id
+        since_id = max(tweet.id, since_id)
+        reply_to_mention(api, tweet=tweet, keywords=keywords)
+    return since_id
 
-
-# Define a fav_retweet function that accepts api, create terms string to search for and use the tweepy.Cursor object to search those terms 100 times
-def fav_retweet(api):
-    '''
-    This function search for tweets in the with a search criteria
-    and automatic like the tweet if the tweet has not been liked and
-    retweet the tweet if the tweet has not been retweeted
-    '''
-    search = ["#ZTM", "#Zerotomastery", "#ztm", "zerotomastery",
-              "ZeroToMastery", "Andrei Neagoie", "Yihua Zhang", "Daniel Bourke"]
-    for tweet in tweepy.Cursor(api.search, search).items(25):
-        try:
-            if not tweet.favorite():
-                tweet.favorite()
-                print("I have liked the tweet")
-            if not tweet.retweet():
-                tweet.retweet()
-                print('Retweeted the tweet')
-        except tweepy.TweepError as e:
-            print(e.reason)
-        except StopIteration:
-            break
-
+# Define a check_mentions function that accepts api, keywords, and since_id, follow and reply to the user if user has mentioned us
+# def check_mentions(api, keywords, since_id):
+#     new_since_id = since_id
+#     for tweet in tweepy.Cursor(api.mentions_timeline,
+#                                since_id=since_id).items():
+#         new_since_id = max(tweet.id, new_since_id)
+#         try:
+#             if tweet.in_reply_to_status_id is not None:
+#                 # Tweet is a reply
+#                 break
+#             elif any(keyword in tweet.text for keyword in keywords):
+#                 status = '@' + tweet.user.screen_name + \
+#                 ' Zero To Mastery, ZTMBot to the rescue! zerotomastery.io/'
+#                 api.update_status(
+#                 status=status, in_reply_to_status_id=tweet.id_str)
+#                 print('replied to', tweet.user.screen_name)
+#                 time.sleep(900)
+#             else:
+#                 pass
+#         except tweepy.TweepError as e:
+#             print("Error replying", e)
+#     return new_since_id
 
 # Define a main function to connect to the api and create a since_id counter, call all above functions
 def main():
     api = create_api()
-    since_id = 1
+    reply_since_id = 1
+    # since_id = 1
     while True:
         # check_mentions(api, ["ZtmBot", "ztmBot", "@ZtmBot"], since_id)
         follow_followers(api)
         unfollow(api)
-        # fav_retweet(api)
+        reply_to_mentions_since_id(api, ["ZtmBot", "ztmBot", "@ZtmBot"], reply_since_id)
         time.sleep(60)
 
 
