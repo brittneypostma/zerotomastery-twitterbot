b40dd76072e000185d523d575aa611fb1f9e9b93
Brittney <brittney.postma@hotmail.com>
2020-02-24 13:13:03 -0500

    refined print statement

--- ztm-bot.py
+++ ztm-bot.py
@@ -39,19 +39,19 @@ class Stream_Listener(tweepy.StreamListener):
     #     screen_name = data.get('user', {}).get('screen_name', '')
     #     status_id = data.get('id_str', None)
 
-    #     try:
-    #         if data.user.screen_name.lower() == self.data.user.screen_name.lower():
-    #             return
-    #         elif data.user.screen_name.lower() == 'ztmbot':
-    #             print('[StreamListener] incoming tweet from %s (@%s):' %
-    #                   (user_name, screen_name))
-    #             print('[StreamListener] '+repr(text))
-    #             print('[StreamListener] Replied to', data.user.screen_name)
-    #             status = (f'@{screen_name} Zero To Mastery, ZTMBot to'
-    #                       ' the rescue!\nzerotomastery.io/')
-    #             self.dispatcher.tweet(text=status, in_reply_to=status_id)
-    #     except tweepy.TweepError as error:
-    #         print(error, text)
+        # try:
+        #     if data.user.screen_name.lower() == self.data.user.screen_name.lower():
+        #         return
+        #     elif data.user.screen_name.lower() == 'ztmbot':
+        #         print('[StreamListener] incoming tweet from %s (@%s):' %
+        #               (user_name, screen_name))
+        #         print('[StreamListener] '+repr(text))
+        #         print('[StreamListener] Replied to', data.user.screen_name)
+        #         status = (f'@{screen_name} Zero To Mastery, ZTMBot to'
+        #                   ' the rescue!\nzerotomastery.io/')
+        #         self.dispatcher.tweet(text=status, in_reply_to=status_id)
+        # except tweepy.TweepError as error:
+        #     print(error, text)
 
     def on_status(self, tweet):
         """Checks the status of the tweet. Mark it as favourite if not already done it and retweet if not already
@@ -59,7 +59,7 @@ class Stream_Listener(tweepy.StreamListener):
 
         :param tweet: tweet from listening to the stream
         """
-        print(self, tweet)
+        print('self:', self, 'tweet:', tweet)
         # try:
         #     keywords = ["ZtmBot", "ztmBot", "@ZtmBot"]
         #     if tweet.user.id == api.me().id or tweet.in_reply_to_status_id:
