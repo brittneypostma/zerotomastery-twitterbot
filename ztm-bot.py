06353aace3a5b133632eaa5e251e11e677f331d2
Brittney <brittney.postma@hotmail.com>
2020-02-24 10:48:29 -0500

    testing reply to mentions in stream

--- ztm-bot.py
+++ ztm-bot.py
@@ -21,6 +21,25 @@ class Stream_Listener(tweepy.StreamListener):
         if tweet.in_reply_to_status_id is not None or tweet.user.id == self.me.id:
             # This tweet is a reply or I'm its author so, ignore it
             return
+        if 'ZTMBot' in tweet:
+            try:
+                if tweet.user.id == api.me().id or tweet.in_reply_to_status_id:
+                    return None
+                text = tweet.text.lower()
+                print(
+                    f'Checking tweet {tweet.id} by @{tweet.user.screen_name}...')
+                if not keywords or any(keyword.lower() in text for keyword in keywords):
+                    print(f'\n\nReplying to tweet {tweet.id} by @'
+                          f'{tweet.user.screen_name}...')
+                    status = (f'@{tweet.user.screen_name} Zero To Mastery, ZTMBot to'
+                              ' the rescue!\nzerotomastery.io/')
+                    api.update_status(status=status,
+                                      in_reply_to_status_id=tweet.id_str,
+                                      auto_populate_reply_metadata=True)
+                    print('Replied to', tweet.user.screen_name)
+                    time.sleep(10)
+            except tweepy.TweepError as e:
+                print("Error replying", e)
         if not tweet.favorited:
             # Mark it as Liked, since we have not done it yet
             try:
@@ -69,5 +88,5 @@ if __name__ == '__main__':
     # third for Daniel Bourke
     follow_list = ["224115510", "2998698451", "743086819"]
     keywords = ["#ZTM", "#Zerotomastery", "#ztm", "zerotomastery",
-                "ZeroToMastery", "Andrei Neagoie", "Yihua Zhang", "Daniel Bourke"]
+                "ZeroToMastery", "Andrei Neagoie", "Yihua Zhang", "Daniel Bourke", "@ZTMBot"]
     main(follow_list, keywords)
